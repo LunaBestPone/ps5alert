b4aeed360a8225b4b4a664462ef626e10d1e97a5
LunaBestPone <saltyprinx@gmail.com>
2020-11-19 17:28:47 -0500

    final adjustment to the scripts

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # ps5alert
 A naïve implementation of ps5 direct page auto refresher, works only for windows. Tried to use Selenium on unix but of course they have detection for that.
 # Usage
+1. install [Buster](https://chrome.google.com/webstore/detail/buster-captcha-solver-for/mpbjkejclgfgadiemmefgebjfooflfhl?hl=en) chrome extension
+1. Go to options of Buster, Switch to Google Cloud Speech to Text API, insert your own API key (Guide on this will not be covered). If you have the 300 dollars free credit on Google Cloud, this will be most likely free over your usage during the script.
 1. Run
   ```pip install -r requirements.txt```
-2. Launch chrome with [ps direct](https://direct.playstation.com/en-us/ps5) tab open on top
+2. Launch chrome with [ps direct](https://direct.playstation.com/en-us/ps5) tab open on top, for ```main_1920x1080.py``` variant, you need to also open chrome settings tap, place it on the second position.
 3. Run
   ```python main.py```
   or
@@ -14,4 +16,7 @@ A naïve implementation of ps5 direct page auto refresher, works only for window
 ## main.py
 Makes beeps if hits the queue page, requires a human to complete recaptcha even if it's not an actual queue. Exit execution by killing the program whenever you like.
 ## main_1920x1080.py
-Makes beeps for one minute if hits an actual queue or the program just broke. Doesn't require a human to complete recaptcha if it's just bot checking. Also scrolls to a position where you can confirm the stock status after redirections. Exit execution by killing the program whenver you like. This only works for 1920x1080 resolution devices with windows task bar positioned on top because the mouse operation coordinates are hard coded. You can adjust the value, start by using ```findmouse.py``` which provides information on your mouse coordinates in real time.
+Makes beeps for one minute if hits an actual queue or the program just broke. Doesn't require a human to complete recaptcha if it's just bot checking. Exit execution by killing the program whenver you like. This only works for 1920x1080 resolution devices with windows task bar positioned on top because the mouse operation coordinates are hard coded. You can adjust the value, start by using ```findmouse.py``` which provides information on your mouse coordinates in real time.
+### Note
+1. You will most likely need adjustments for clicking coordinates for the 1920x1080 version. But it is the variant that bought me the video game machine. The comments in the script is detailed enough for localization on different environment (of course you need matching operating system).
+2. Remember to deactivate your GC API key after you are done with these stuffs. I do not have responsibility for your bills.
--- main_1920x1080.py
+++ main_1920x1080.py
@@ -11,38 +11,83 @@ def main():
     app = Application(backend='uia')
     app.connect(title_re='.*Chrome.*')
     dlg = app.top_window()
-    encounter = False
 
     while True:
-        time.sleep(random.uniform(3, 5))
-        send_keys('{ENTER}')
+        # how long you want the refresh interval? set it here
+        time.sleep(20)
         url = dlg.child_window(title="Address and search bar", control_type="Edit").get_value()
         if 'direct-queue' in url:
-            # consecutive hits on queue means either failed execution or hittin an actual queue
-            if encounter:
-                for i in range(60):
-                    print('\a')
-                    time.sleep(1)
-                sys.exit('Alerting done, shutting down.')
-
-            # x and y positions for recaptcha check box, modify this if you have different screen config
+            # x and y positions for BOT CHECKING recaptcha check box, modify this if you have different screen config
             randx = int(random.uniform(430,453))
             randy = int(random.uniform(628,649))
             click(button='left', coords=(randx,randy))
-            encounter = True
+            # wait for captcha box to appear, or redirect directly to ps direct
+            time.sleep(2)
+            url = dlg.child_window(title="Address and search bar", control_type="Edit").get_value()
+            if 'direct-queue' in url:
+                # Buster extension kicks in, this is the box for buster button after recaptcha box appears
+                click(button='left', coords=(606,930))
+                # wait for verification, and potential redirect
+                time.sleep(3)
+                # if still on the direct queue page, it means either 1. current captcha failed 2. google detected you are a bot
+                url = dlg.child_window(title="Address and search bar", control_type="Edit").get_value()
+                if 'direct-queue' in url:
+                    # attemp to click on 'get new challenge'
+                    click(button='left', coords=(501,771))
+                    time.sleep(0.5)
+                    # this position overlaps both buster button position, and refresh captcha box button if google detects the botting
+                    click(button='left', coords=(630,771))
+                    time.sleep(3)
+                    
+                    # if google detection happened and bot clicked refresh, need to open up the recaptcha box again
+                    # x and y positions for bot checking recaptcha check box, modify this if you have different screen config
+                    randx = int(random.uniform(430,453))
+                    randy = int(random.uniform(628,649))
+                    click(button='left', coords=(randx,randy))
+
+                    # Buster kicks in again
+                    time.sleep(1)
+                    click(button='left', coords=(606,930))
+                    time.sleep(3)
+
+                    # click on first tap addres bar and paste in ps5 direct link
+                    click(button='left', coords=(385,90))
+                    send_keys('^v')
+                    send_keys('{ENTER}')
+                    time.sleep(3)
+                # if passed bot checking, still meet direct queue page afterwards, that means you hit the queue
+                time.sleep(2)
+                url = dlg.child_window(title="Address and search bar", control_type="Edit").get_value()
+                if 'direct-queue' in url:
+                    # try to queue the queue before alert happens
+                    randx = int(random.uniform(430,453))
+                    randy = int(random.uniform(409,429))
+                    click(button='left', coords=(randx, randy))
+                    # alert for 1 min
+                    for i in range(60):
+                        print('\a')
+                        time.sleep(1)
+                    sys.exit('Alerting done, shutting down.')
+            # move on to next cycle if recaptcha didn't appear at all (restart from top of while loop)
             continue
 
-        encounter = False
-        # this is the position of scroll bar
-        # modify the coordinates if you have different screen config
-        press(button='left', coords=(1910,250))
-        time.sleep(0.2)
-        release(button='left', coords=(1910,250))
-        press(button='left', coords=(1910,460))
-        time.sleep(0.2)
-        release(button='left', coords=(1910,460))
+        # add a step for clearing cache, since sony's site queue users based on their cookie time stamp, doing this gets you the newest cookies
+        # click on second tap which is chrome setting
+        click(button='left', coords=(388,56))
+        time.sleep(0.1)
+        # click on clear browser data sub-section
+        click(button='left', coords=(784,1009))
+        time.sleep(0.1)
+        # click on clear cache button
+        click(button='left', coords=(1198,749))
+        time.sleep(0.1)
+        # click on first tap
+        click(button='left',coords=(181,59))
+        
+        # address bar pasting
+        click(button='left', coords=(385,90))
+        send_keys('^v')
         send_keys('{ENTER}')
-        send_keys('^{F5}')
 
 
 
